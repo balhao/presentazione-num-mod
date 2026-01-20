eb1d322b09385df1b8914e31f23d161169767544
balhao <balhao@users.noreply.github.com>
2026-01-20 21:23:36 +0000

    deploy: 0ead920b28a2035b30109ed828d8335fcc27510a

--- assets/RestartOnEnter-CZijlMMt.js
+++
@@ -1,1 +0,0 @@
-import{r as s,K as a,j as o,i as n}from"./modules/vue-Bg1QFOsN.js";import"./index-CQFNMHGq.js";import{u}from"./slidev/context-u1ZAowZ4.js";const _={__name:"RestartOnEnter",setup(i){const{isVisible:r}=u(),e=s(!0);return a(r,t=>{t&&(e.value=!1,setTimeout(()=>{e.value=!0},10))}),(t,m)=>e.value?o(t.$slots,"default",{key:0}):n("v-if",!0)}};export{_};
